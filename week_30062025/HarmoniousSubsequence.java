eee2ef9771e88891be2d24ed294e266e921fefac
iestynmullinor <iestynmullinor@gmail.com>
2025-06-30 20:33:28 +0100

    fix

--- week_30062025/HarmoniousSubsequence.java
+++ week_30062025/HarmoniousSubsequence.java
@@ -1,23 +1,20 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class HarmoniousSubsequence {
 
     public static int findLHS(int[] nums) {
-        TreeMap<Integer, Integer> counts_map = new TreeMap<>();
+        HashMap<Integer, Integer> counts_map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
             counts_map.put(nums[i], counts_map.getOrDefault(nums[i], 0) + 1);
         }
 
         int longest = 0;
-        List<Map.Entry<Integer, Integer>> counts_list = new ArrayList<>(counts_map.entrySet());
 
-        for (int i = 0; i < counts_list.size() - 1; i++) {
-            if (counts_list.get(i).getKey() - counts_list.get(i + 1).getKey() == -1) {
-                int length = counts_list.get(i).getValue() + counts_list.get(i + 1).getValue();
+        for (Map.Entry<Integer, Integer> entry : counts_map.entrySet()) {
+            if (counts_map.containsKey(entry.getKey()+1)) {
+                int length = entry.getValue() + counts_map.get(entry.getKey()+ 1);
                 if (length > longest)
                     longest = length;
             }
